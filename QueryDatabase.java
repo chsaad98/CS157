22f3335c92267ea50f5024ca2963139cd589238b
chsaad98 <143139058+chsaad98@users.noreply.github.com>
2024-09-04 19:52:39 -0700

    Create QueryDatabase.java

---
+++ QueryDatabase.java
@@ -0,0 +1,38 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class QueryDatabase {
+
+    public static void main(String[] args) {
+        String jdbcURL = "jdbc:mysql:;
+        String username = "root";
+        String password = "Munazza123@";
+
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(jdbcURL, username, password);
+
+
+            String sql = "SELECT gender, AVG(height_cm) AS avg_height FROM AgePrediction GROUP BY gender ORDER BY avg_height DESC LIMIT 1";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                String gender = resultSet.getString("gender");
+                float avgHeight = resultSet.getFloat("avg_height");
+                System.out.println("Gender with highest average height: " + gender + " (Average Height: " + avgHeight + " cm)");
+            }
+
+            resultSet.close();
+            statement.close();
+            connection.close();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
