4ba778454c2eee8db59ab8c48df1045401c4a944
chsaad98 <143139058+chsaad98@users.noreply.github.com>
2024-09-04 19:58:52 -0700

    Update QueryDatabase.java

--- QueryDatabase.java
+++ QueryDatabase.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class QueryDatabase {
 
     public static void main(String[] args) {
-        String jdbcURL = "jdbc:mysql:;
+        String jdbcURL = "jdbc:mysql://localhost:3306/HumanAgePrediction";
         String username = "root";
         String password = "Munazza123@";
 
